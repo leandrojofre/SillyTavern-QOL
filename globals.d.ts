46dd9b2069b1d8e10a11bc8c4b1e0eb0ef3fca34
leandrojofre <leandrotomasjofre@gmail.com>
2026-04-03 08:14:36 -0300

    Update ESLint and JSDocs types definitions

---
+++ globals.d.ts
@@ -0,0 +1,42 @@
+export {}
+
+declare global {
+    type ExtensionSettingsFeatures = {
+        quickRegenerate: boolean;
+        quickRegenerateAutoHide: boolean;
+        playErrorSound: boolean;
+        zoomCharacterAvatar: boolean;
+        simpleUserInput: boolean;
+        showActivatedWiEntries: boolean;
+        collapseNewlines: boolean;
+        preventPageNavigation: boolean;
+    };
+
+    type ExtensionSettings = {
+        enabled: boolean;
+        soundVolume: number;
+        features: ExtensionSettingsFeatures;
+        customSamplers: Record<string, any>;
+        debug: boolean;
+    };
+
+    type WIEntry = {
+        uid: number;
+        world: string;
+        comment: string;
+        content: string;
+        outletName: string;
+        displayIndex: number;
+        vectorized: boolean;
+        constant: boolean;
+    };
+
+    type ScannedWIEntries = {
+        activated?: {
+            entries: Map<string, Record<string, any>>;
+        };
+        new?: {
+            successful: WIEntry[];
+        };
+    };
+}
